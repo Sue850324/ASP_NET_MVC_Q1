f82e7fff78888935a8bf3c59198c2a1eaf1f5bcf
suec <suec@bqool.com>
2019-04-18 17:49:12 +0800

    加入

---
+++ ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASP_NET_MVC_Q1.Models;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+
+namespace ASP_NET_MVC_Q1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Detail()
+        {           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Detail(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["Name"]  = user.name;
+                Session["Phone"]  = user.phone;
+                Session["Email"]  = user.email;
+                return RedirectToAction("Check");
+            }
+            return View();               
+        }
+
+        public ActionResult Check()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Check(FileModel file)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["FileName"] = file.fileName;
+                string writefile = "Name:" + '\n' + Session["Name"] + "Phone:" + Session["Phone"] + "\n" + "Email:" + Session["Email"];
+                string sourceFile = Server.MapPath("~/App_Data/"+ Session["FileName"] + ".txt") ;
+
+                System.IO.File.WriteAllText(sourceFile, writefile);
+
+                return RedirectToAction("Save");
+            }
+            return View();
+        }
+
+        public ActionResult Save(UserModel param)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC_Q1.Models { 
+    public class UserModel
+    {
+        [Required(ErrorMessage = "*此為必填欄位!")]
+        [StringLength(20,MinimumLength =2,ErrorMessage ="至少需要2個字元")]
+        public string name { set; get; }
+
+        [Required(ErrorMessage = "請輸入手機號碼")]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3}$",ErrorMessage="需為09xx-xxx-xxx")]
+        public string phone { set; get; }
+
+        [EmailAddress(ErrorMessage = "格式錯誤")]
+        [Required(ErrorMessage ="請輸入Email")]      
+        public string email { set; get; }
+
+    }
+}
