6a583867a1d5ccfa2d7b13915a299460beb7a629
suec <suec@bqool.com>
2019-04-22 15:06:50 +0800

    傳遞資料方式修改成post

---
+++ ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASP_NET_MVC_Q1.Models;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+
+namespace ASP_NET_MVC_Q1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        public ActionResult Check()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Check(UserModel user)
+        {
+
+            UserModel userModel = new UserModel();
+
+            userModel.name = user.name;
+            userModel.email = user.email;
+            userModel.phone = user.phone;
+
+            return View(userModel);
+        }
+
+
+    }
+}
---
+++ ASP_NET_MVC_Q1/ASP_NET_MVC_Q1/Controllers/DataController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASP_NET_MVC_Q1.Models;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+
+namespace ASP_NET_MVC_Q1.Controllers
+{
+    public class DataController : Controller
+    {
+        public ActionResult Save(UserModel user)
+        {
+            UserModel userModel = new UserModel();
+            userModel.name = user.name;
+            userModel.email = user.email;
+            userModel.phone = user.phone;
+
+            string writefile = "Name:" + '\n' + userModel.name + "Phone:" + userModel.phone + "\n" + "Email:" + userModel.email;
+            string sourceFile = Server.MapPath("~/App_Data/" + "suec.txt");
+            System.IO.File.WriteAllText(sourceFile, writefile);
+
+            return View(userModel);
+        }
+    }
+}
